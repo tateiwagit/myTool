bde9a034562c76db0e8925b47e22c69fd6ad0536
tateiwagit <tatitatagoo@gmail.com>
2023-08-26 20:48:37 +0900

    それぞれの画像の大まかなプロトタイプを作成した。 ビューワー画面の画像タイトルラベルの位置がうまくいってないが、後回しとする。

--- main.py
+++ main.py
@@ -3,40 +3,58 @@ from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager, NoTransition
 from kivy.lang import Builder
 
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+
 # Kivyの設定
-kivy.config.Config.set('graphics', 'width', '400')
-kivy.config.Config.set('graphics', 'height', '300')
+# kivy.config.Config.set('graphics', 'width', '400')
+# kivy.config.Config.set('graphics', 'height', '300')
 
 #6つの画面
-##class HomeScreen
+class HomeScreenApp(Screen):
+    pass
+class ProjectScreenApp(Screen):
+    pass
+class BrowseScreenApp(Screen):
+    pass
+class DownloadScreenApp(Screen):
+    pass
+class CollectScreenApp(Screen):
+    pass
+class ViewScreenApp(Screen):
+    pass
 
 # メインのアプリケーションクラス
-class Init_App(App):
+class InitApp(App):
+    sm = ScreenManager()
+
     def build(self):
-        self.sm = ScreenManager(transition=NoTransition())
+        #self.sm = ScreenManager(transition=NoTransition())
         
         # kv ファイルを読み込む
         Builder.load_file('HomeScreen.kv')
-        # Builder.load_file('Projects.kv')
-        # Builder.load_file('Browse.kv')
-        # Builder.load_file('Downloading.kv')
-        # Builder.load_file('Collections.kv')
-        # Builder.load_file('Viewer.kv')
-        screen = Screen(name='home')
+        Builder.load_file('ProjectScreen.kv')
+        Builder.load_file('BrowseScreen.kv')
+        Builder.load_file('DownloadScreen.kv')
+        Builder.load_file('CollectScreen.kv')
+        Builder.load_file('ViewScreen.kv')
+        # screen = Screen(name='home')
 
-        self.sm.add_widget(screen)
-        # sm.add_widget(TwoScreen(name='two'))
-        # sm.add_widget(ThreeScreen(name='three'))
-        # sm.add_widget(FourScreen(name='four'))
-        # sm.add_widget(FiveScreen(name='five'))
-        # sm.add_widget(SixScreen(name='six'))
+        self.sm.add_widget(HomeScreenApp(name='home'))
+        self.sm.add_widget(ProjectScreenApp(name='project'))
+        self.sm.add_widget(BrowseScreenApp(name='browse'))
+        self.sm.add_widget(DownloadScreenApp(name='download'))
+        self.sm.add_widget(CollectScreenApp(name='collect'))
+        self.sm.add_widget(ViewScreenApp(name='view'))
+
+        self.sm.current = 'view'
+
 
         return self.sm
 
     def switch_screen(self, screen_name):
-        sm = self.root
-        sm.current = screen_name
+        self.sm.current = screen_name
 
 if __name__ == "__main__":
-    Init_App().run()
+    InitApp().run()
 
